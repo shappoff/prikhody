118c887439c2b7b49ecbc73f1d5ee7ed5e48eab9
Sergey Shappo <shappoff@gmail.com>
2025-10-28 21:55:35 +0300

    update project

--- src/components/paths.tsx
+++ src/components/paths.tsx
@@ -1,18 +1,18 @@
 import path from "path";
 
-export const statistics333projectPath = path.resolve(`src/app/niab/store/333-9.json`);
-export const mainFODDataPath = path.resolve(`src/app/niab/store/data.json`);
-export const indexedNIABDataPath = path.resolve(`src/app/niab/store/indexedNIABData.json`);
-export const rejectedFormattedPath = path.resolve(`src/app/niab/store/rejectedFormattedPath.json`);
+export const statistics333projectPath = path.resolve(`src/app/prikhody/store/333-9.json`);
+export const mainFODDataPath = path.resolve(`src/app/prikhody/store/data.json`);
+export const indexedNIABDataPath = path.resolve(`src/app/prikhody/store/indexedNIABData.json`);
+export const rejectedFormattedPath = path.resolve(`src/app/prikhody/store/rejectedFormattedPath.json`);
 
 export const digitedFormattedDataPath = path.resolve(`src/app/prikhody/store/digitedFormattedData.json`);
 export const prikhodyArchivesDataPath = path.resolve(`src/app/prikhody/store/prikhodyArchivesData.json`);
 export const prikhodyMainDataPath = path.resolve(`src/app/prikhody/store/prikhodyMainData.json`);
 
-export const catalogarchivesgovPath = path.resolve(`src/app/catalogarchivesgov/all_merged.json`);
-export const catalogarchivesgovBelarusPath = path.resolve(`src/app/catalogarchivesgov/belarus.json`);
-export const catalogarchivesgovSmolenskPath = path.resolve(`src/app/catalogarchivesgov/smolensk/smolensk.json`);
+export const catalogarchivesgovPath = path.resolve(`src/app/prikhody/all_merged.json`);
+export const catalogarchivesgovBelarusPath = path.resolve(`src/app/prikhody/belarus.json`);
+export const catalogarchivesgovSmolenskPath = path.resolve(`src/app/prikhody/smolensk/smolensk.json`);
 
-export const cgia_19_127Path = path.resolve(`src/app/cgia_19_127/store/cgia_19_127.json`);
+export const cgia_19_127Path = path.resolve(`src/app/prikhody/store/cgia_19_127.json`);
 
-export const orthodox_catholicNameDataPath = path.resolve(`src/app/names/store/orthodox_catholicNameDataPath.json`);
+export const orthodox_catholicNameDataPath = path.resolve(`src/app/prikhody/store/orthodox_catholicNameDataPath.json`);
